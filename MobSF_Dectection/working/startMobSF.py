03979951e85b8be25d60ba2e25d4844ff538ed68
JeonSeungHyeok <victory_hyeok@naver.com>
2025-02-09 15:07:22 +0900

    python convention

--- MobSF_Dectection/working/startMobSF.py
+++ MobSF_Dectection/working/startMobSF.py
@@ -4,24 +4,23 @@ import platform
 import psutil
 from colors import *
 
-
 class Starting:
-    def __init__(self, mobsfName) -> None:
+    def __init__(self, mobsf_name) -> None:
         self.process = None
-        self.mobsfName = mobsfName
+        self.mobsf_name = mobsf_name
 
     def start_mobsf(self):
-        print(f"{GREEN}[*]{RESET} Finding MobSF dir path")
-        mobsfDirPath = os.path.join(os.getcwd(), self.mobsfName)
-        runScript = ''    
-        if platform.system()=='Windows':
-            runScript='run.bat'
-        elif platform.system()=='Darwin':
-            runScript='run.sh'
-        if not os.path.exists(os.path.join(mobsfDirPath, runScript)):
-            print(f"{RED}[-]{RESET} Error: The {runScript} is not in this path {mobsfDirPath}")
+        print(f'{GREEN}[*]{RESET} Finding MobSF dir path')
+        mobsf_dir_path = os.path.join(os.getcwd(), self.mobsf_name)
+        run_script = ''    
+        if platform.system() == 'Windows':
+            run_script = 'run.bat'
+        elif platform.system() == 'Darwin':
+            run_script = 'run.sh'
+        if not os.path.exists(os.path.join(mobsf_dir_path, run_script)):
+            print(f'{RED}[-]{RESET} Error: The {run_script} is not in this path {mobsf_dir_path}')
             return
-        self.process = subprocess.Popen(os.path.join(mobsfDirPath, runScript), shell=True, cwd=mobsfDirPath)
+        self.process = subprocess.Popen(os.path.join(mobsf_dir_path, run_script), shell=True, cwd=mobsf_dir_path)
         print(f'{BLUE}[+]{RESET} MobSF Started Successfully')        
         return self.process
 
@@ -33,7 +32,7 @@ class Starting:
                 child.terminate()
             self.process.terminate()
             self.process.wait()
-        print(f"{RED}[*]{RESET} Byebye~")
+        print(f'{RED}[*]{RESET} Byebye~')
 
 
 
